094c77035df2c3048efafcfcae828f88d5f19cca
Arya1424 <144880167+Arya1424@users.noreply.github.com>
2025-06-08 11:56:29 +0530

    Time: 736 ms (53.16%) | Memory: 18.5 MB (29.03%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,35 +1,29 @@
 class Solution(object):
     def threeSum(self, nums):
-        nums.sort()  # sort once upfront
-        result = []
-
+        #2 pointer
+        nums.sort()
+        result=[]
         for i in range(len(nums)):
-            # Skip duplicate values for i to avoid duplicate triplets
-            if i > 0 and nums[i] == nums[i-1]:
+            if i>0 and nums[i]==nums[i-1]:
                 continue
+            left=i+1
+            right=len(nums)-1
 
-            left = i + 1
-            right = len(nums) - 1
+            while left<right:
+                total=nums[i]+nums[left]+nums[right]
 
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
+                if total==0:
+                    result.append([nums[i],nums[left],nums[right]])
+                    left+=1
+                    right-=1
 
-                if total == 0:
-                    result.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    right -= 1
-
-                    # Skip duplicates for left pointer
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-
-                    # Skip duplicates for right pointer
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-
-                elif total < 0:
-                    left += 1  # Need a bigger sum, move left up
+                    while left<right and nums[left]==nums[left-1]:
+                        left+=1
+                    while left<right and nums[right]==nums[right+1]:
+                        right-=1
+                    
+                elif total<0:
+                    left+=1
                 else:
-                    right -= 1  # Need a smaller sum, move right down
-
+                    right-=1
         return result
