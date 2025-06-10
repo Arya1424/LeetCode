ef3faf384bd1847fbade5160b5ebd065c443c15f
Arya1424 <144880167+Arya1424@users.noreply.github.com>
2025-06-10 14:31:05 +0530

    Time: 11 ms (55.16%) | Memory: 12.7 MB (19.16%) - LeetSync

---
+++ 38-count-and-say/count-and-say.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def countAndSay(self, n):
+        result="1"
+        for i in range(1,n):
+            current=""
+            count=1
+            for j in range(1,len(result)):
+                if result[j]==result[j-1]:
+                    count+=1
+                else:
+                    current+=str(count)+result[j-1]
+                    count=1
+            current+=str(count)+result[-1]
+            result=current
+        return result
+
+        
