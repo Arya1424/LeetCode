c430c431b4adfdc8addda136a88d6205930ff271
Arya1424 <144880167+Arya1424@users.noreply.github.com>
2025-08-29 17:44:18 +0530

    Time: 119 ms (35.96%) | Memory: 20.7 MB (54.93%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,15 +1,13 @@
 class Solution(object):
     def maxArea(self, height):
-        maxvol=0
-        left=0
-        right=len(height)-1
-        while left<right:
-            vol=min(height[left],height[right])*(right-left)
-            if vol>maxvol:
-                maxvol=vol 
-            if height[left]<height[right]:
-                left+=1
+        l,r=0,len(height)-1
+        water=0
+        while l<r:
+            cont=min(height[l],height[r])*(r-l)
+            water=max(water,cont)
+            if height[l]>height[r]:
+                r-=1
             else:
-                right-=1                 
-        return maxvol
-        
+                l+=1
+
+        return water
