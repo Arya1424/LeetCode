e2db5ed04f4572552249556abd1d3294cea0fda1
Arya1424 <144880167+Arya1424@users.noreply.github.com>
2025-10-06 12:56:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (50.69%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,14 +1,16 @@
 class Solution(object):
     def subsets(self, nums):
         n=len(nums)
-        res,sol=[],[]
+        res, sol=[], []
+
         def backtrack(i):
             if i==n:
                 res.append(sol[:])
-                return 
+                return
             backtrack(i+1)
             sol.append(nums[i])
             backtrack(i+1)
             sol.pop()
         backtrack(0)
         return res
+        
