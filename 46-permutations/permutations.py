befbe8139adbced0b9179a3e0f0a2fd9f921a6f6
Arya1424 <144880167+Arya1424@users.noreply.github.com>
2025-12-09 13:25:09 +0530

    Time: 3 ms (33.12%) | Memory: 17.8 MB (95.52%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,12 +1,13 @@
-class Solution(object):
-    def permute(self, nums):
-        result=[]
-        def backtrack(path,remaining):
-            if not remaining:
-                result.append(path)
-                return
-            for i in range(len(remaining)):
-                backtrack(path+[remaining[i]],remaining[:i]+remaining[i+1:])
-        backtrack([],nums)
-        return result
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(start, end):
+            if start==end:
+                res.append(nums[:])
+            for i in range(start,end):
+                nums[start], nums[i]=nums[i], nums[start]
+                backtrack(start+1, end)
+                nums[start], nums[i]=nums[i], nums[start]
+        res=[]
+        backtrack(0, len(nums))
+        return res
         
