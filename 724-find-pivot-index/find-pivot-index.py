53560daf8c7656bca0624eb362016a44c319e923
Aryasree M <144880167+Arya1424@users.noreply.github.com>
2026-01-28 13:05:28 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (31.36%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,9 +1,9 @@
-class Solution(object):
-    def pivotIndex(self, nums):
-        left_sum=0
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left=0
         s=sum(nums)
-        for i,num in enumerate(nums):
-            if left_sum==s-left_sum-num:
+        for i, num in enumerate(nums):
+            if left==s-left-num:
                 return i
-            left_sum+=num
+            left+=num
         return -1
